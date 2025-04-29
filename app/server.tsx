a51ec603912987fcb46324d73f7a494d70e885a1
t0dorakis <theodor.hillmann@funkemedien.de>
2025-04-30 01:50:02 +0200

    refactor: meta, add registry to the body, bang style in head

--- app/server.tsx
+++ app/server.tsx
@@ -2,9 +2,8 @@
 import { eventHandler } from "vinxi/http";
 import { getManifest } from "vinxi/manifest";
 import h from "../lib/JSX"; // Explicitly import your JSX factory
-import HomePage from "./pages/HomePage";
 import Stone from "../lib/Stone";
-
+import HomePage from "./pages/HomePage";
 export default eventHandler({
   handler: async (event) => {
     const clientManifest = getManifest("client");
@@ -17,21 +16,27 @@ export default eventHandler({
     const clientEntry =
       clientManifest.inputs[clientManifest.handler].output.path;
 
-    event.node.res.setHeader("Content-Type", "text/html");
-
-    // for now this needs to be called before the registry is set to the window
-    const page = HomePage();
-
     return (
       <html lang="en">
         <head>
-          <title>
-            Stone Throw – A simple framework for building web components with
-            server-side rendering
-          </title>
+          <title>Stone Throw</title>
+          <meta
+            name="description"
+            content="A simple framework for building web components with
+            server-side rendering"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
 
           {/* Include all client assets (CSS, preloads, etc.) */}
           {clientAssets.map((asset) => {
+            // bang the style directly into the head
+            if (asset.tag === "style") {
+              return <style>{asset.children}</style>;
+            }
+
             if (asset.tag === "link" && asset.attrs?.href) {
               return <link key={asset.attrs.href} {...asset.attrs} />;
             }
@@ -39,19 +44,18 @@ export default eventHandler({
           })}
 
           <script type="module" src={clientEntry} defer />
-
-          {/* Hand the serverside registry keys over to the client */}
-          <script type="module">
-            {`
+        </head>
+        {HomePage()}
+        {/* Hand the serverside registry keys over to the client */}
+        <script type="module">
+          {`
               window.FRAMEWORK = {
                 componentsToRegister: ${JSON.stringify(
                   Stone.getComponentsToRegister()
                 )}
               };
             `}
-          </script>
-        </head>
-        {page}
+        </script>
       </html>
     );
   },
